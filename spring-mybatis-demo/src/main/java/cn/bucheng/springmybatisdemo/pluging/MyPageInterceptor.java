c3ff3d34c6c2396331031a34d78c837c515bcd06
yinbucheng <yin.chong@intellif.com>
2020-02-11 17:27:25 +0800

    添加集成springboot-mybatis测试

--- spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/pluging/MyPageInterceptor.java
+++ spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/pluging/MyPageInterceptor.java
@@ -14,7 +14,7 @@ import java.util.Properties;
         args = {Connection.class, Integer.class/*方法中用到的参数类型*/})})//可以点击拦截对象查看拦截方法的参数，
 public class MyPageInterceptor implements Interceptor {
 
-   private static ThreadLocal<Integer> defaultLimit = new ThreadLocal<>();
+    private static ThreadLocal<Integer> defaultLimit = new ThreadLocal<>();
 
     public static Integer getAndRemoveLimit() {
         try {
@@ -24,6 +24,7 @@ public class MyPageInterceptor implements Interceptor {
         }
     }
 
+
     //    拦截方法，返回结果：目标方法执行后返回的结果
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -32,21 +33,23 @@ public class MyPageInterceptor implements Interceptor {
         //获取要执行的sql命令
         String sql = statementHandler.getBoundSql().getSql();
         System.out.println("要执行的sql命令为：" + sql);
-        if (!sql.toLowerCase().startsWith("select")) {
+        String lower_sql = sql.toLowerCase().trim();
+        if (!lower_sql.startsWith("select")) {
             return invocation.proceed();
         }
-        if (sql.toLowerCase().contains("limit ")) {
+        if (lower_sql.contains("limit ")) {
             return invocation.proceed();
         }
-        defaultLimit.set(20);
+        int defaultSize = 100;
+        defaultLimit.set(defaultSize);
         //为sql语句添加分页
-        sql += " limit 0,20";
+        sql += " limit 0," + defaultSize;
         //元数据对象，可以对原始数据进行操作
         MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
         //重新绑定修改后的sql语句
         metaObject.setValue("boundSql.sql", sql);
         //执行目标方法
-        Object result =  invocation.proceed();
+        Object result = invocation.proceed();
         return result;
     }
 
