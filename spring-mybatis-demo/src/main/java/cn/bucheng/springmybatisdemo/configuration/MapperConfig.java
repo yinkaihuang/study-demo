5cb4df3e905be1614624d486330071e94b912029
yinbucheng <yin.chong@intellif.com>
2020-02-11 19:12:31 +0800

    采用反射保证自定义插件优先级最低

--- spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/configuration/MapperConfig.java
+++ spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/configuration/MapperConfig.java
@@ -2,10 +2,16 @@ package cn.bucheng.springmybatisdemo.configuration;
 
 import cn.bucheng.springmybatisdemo.pluging.MyPageInterceptor;
 import cn.bucheng.springmybatisdemo.pluging.MyResultInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.InterceptorChain;
 import org.apache.ibatis.session.Configuration;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
 
 @org.springframework.context.annotation.Configuration
 public class MapperConfig {
@@ -16,10 +22,29 @@ public class MapperConfig {
         return new ConfigurationCustomizer() {
             @Override
             public void customize(Configuration configuration) {
-                MyPageInterceptor myPlugin1 = new MyPageInterceptor();
-                MyResultInterceptor myPlugin2 = new MyResultInterceptor();
-                configuration.addInterceptor(myPlugin1);
-                configuration.addInterceptor(myPlugin2);
+                //保证myPlugin1和myPlugin2优先级最低
+                MyPageInterceptor myPagePlug = new MyPageInterceptor();
+                MyResultInterceptor resultPlug = new MyResultInterceptor();
+                try {
+                    Field field = Configuration.class.getDeclaredField("interceptorChain");
+                    field.setAccessible(true);
+                    InterceptorChain chainList = (InterceptorChain) field.get(configuration);
+                    List<Interceptor> interceptors = chainList.getInterceptors();
+                    LinkedList<Interceptor> tempList = new LinkedList<>();
+                    tempList.add(myPagePlug);
+                    tempList.add(resultPlug);
+                    tempList.addAll(interceptors);
+                    Field chainField = InterceptorChain.class.getDeclaredField("interceptors");
+                    chainField.setAccessible(true);
+                    List<Interceptor> interceptorList = (List<Interceptor>) chainField.get(chainList);
+                    interceptorList.clear();
+                    for (Interceptor temp : tempList) {
+                        interceptorList.add(temp);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         };
     }
