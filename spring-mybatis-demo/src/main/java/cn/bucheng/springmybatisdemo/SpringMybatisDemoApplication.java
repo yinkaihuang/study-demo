cf3c6877a43d404c89eb4e8b6aff1fd947382507
yinbucheng <yin.chong@intellif.com>
2020-02-13 21:31:06 +0800

    添加根据注解自动注入多数据源

--- spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/SpringMybatisDemoApplication.java
+++ spring-mybatis-demo/src/main/java/cn/bucheng/springmybatisdemo/SpringMybatisDemoApplication.java
@@ -1,10 +1,13 @@
 package cn.bucheng.springmybatisdemo;
 
+import cn.bucheng.springmybatisdemo.annotation.AloneSource;
+import cn.bucheng.springmybatisdemo.annotation.EnableMuchSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@MapperScan("cn.bucheng.springmybatisdemo.mapper")
+@EnableMuchSource(prefix = "custom.mysql",value = {@AloneSource(dbPrefix = "db0",mapperScanPackages = "cn.bucheng.springmybatisdemo.mapper",typeAliasesPackage = "cn.bucheng.springmybatisdemo.domain",mapperLocations = "classpath:mapper/*.xml"),@AloneSource(dbPrefix = "db1",mapperScanPackages = "cn.bucheng.springmybatisdemo.dao",typeAliasesPackage = "cn.bucheng.springmybatisdemo.entity",mapperLocations = "classpath:mapper2/*.xml")})
 public class SpringMybatisDemoApplication {
 
     public static void main(String[] args) {
